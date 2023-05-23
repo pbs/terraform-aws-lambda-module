3b3d346de89b2dd4f2a4df561ef9ab6b82eadff4
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-05-23 09:03:27 -0400

    Fixing Docker test

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,5 +13,8 @@ fi
 # We have to log into ECR now that we're doing Lambdas with containers
 aws ecr get-login-password --region 'us-east-1' | docker login --username AWS --password-stdin "$(aws sts get-caller-identity | jq -r '.Account').dkr.ecr.us-east-1.amazonaws.com"
 
+# We also need access to the public ECR registry
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
 pushd "$GIT_ROOT"/tests >/dev/null || exit 1
 go test -timeout 30m -count=1 -parallel 10 -short ./...
